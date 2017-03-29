3b9b8ee1f75b08cfd5bf30f9e0d2a4d4c2cff8c6
croberts2 <croberts2@luc.edu>
2017-03-29 15:55:33 -0500

    updated wrapper.py to just have the first four steps included

--- wrapper.py
+++ wrapper.py
@@ -2,15 +2,81 @@
 
 import os                           #imports os module to allow the code to use the command line
 import argparse                     #imports the argparse module which accepts input from the command line to use in the running of the code
+
 parser = argparse.ArgumentParser()  #creates the parser list for argparse
 #parser.add_argument("test", help="simply a test") #testing argument
 args = parser.parse_args()          #creates the list of aruments called args
 
-#run Step 1-sex_check.py from Jared (still needs to be not hardcoded)
-os.system("python check_sex.py")    #runs check_sex.py but will need to be changed so it is not hard coded
-print ("Sex check complete.")            #tells the user when the process is complete
+###STEP 1 - Sex Check#####
 
+#make directory step1 for output
+os.system("mkdir out")
+os.system("mkdir out/step1")
+#runs a check sex on data, sends output to step1/step1
+os.system("plink --bfile testdata --check-sex -out out/step1/step1")
+#finds 'PROBLEM' in output
+#this is where the check sex found an issue
+os.system("grep 'PROBLEM' out/step1/step1.sexcheck > out/step1/issues.txt")
 
-# run Step 5-step5.py from Alexa (still needs to be not hardcoded)
-os.system("python step5.py")
-print ("LD prune relationship check and heterozygosity complete.")
+#open issues
+f = open('out/step1/issues.txt', 'r')
+s = f.readlines()
+f.close()
+
+#in each line, only return the family id and sample id
+exclude = []
+for each in s:
+	this = each.split()
+	exclude.append(this[0]+'\t'+this[1])
+	
+	
+#write to testdata/exclude.txt, to be used for down pipe commands	
+o = open('out/step1/exclude.txt', 'w')
+for each in exclude:
+	o.write(each)
+	o.write('\n')
+
+o.close()
+
+###STEP 2 - SNP Call Rate###
+
+#SNP call rate
+#thresholds set to 0.01
+
+#Creates two files: .imiss (individual) and .lmiss (SNP/locus) that details missingness in data
+#excludes data from the sex_check
+#writes data to step2/step2_1/step2_1
+
+os.system("mkdir out/step2")
+os.system("mkdir out/step2/step2_1")
+os.system("plink --bfile testdata --exclude out/step1/exclude.txt --missing --out out/step2/step2_1/step2_1")
+
+#recalculates call rate after removing SNPs with call rates <99% and creates a new set of files
+#excudes data from the sex_check
+#writes data to step2/step2_0/step2_0
+
+os.system("mkdir out/step2/step2_0")
+os.system("plink --bfile testdata --exclude out/step1/exclude.txt --geno 0.01 --make-bed --out out/step2/step2_0/step2_0")
+
+##Creates two files: .imiss (individual) and .lmiss (SNP/locus) that details missingness in data
+#writes data to step2/step2_2/step2_2
+
+os.system("mkdir out/step2/step2_2")
+os.system("plink --bfile out/step2/step2_0/step2_0 --missing --out out/step2/step2_2/step2_2")
+
+### STEP 3 - Person Call Rate ###
+
+#make directory for person call rate check output
+os.system("mkdir out/step3")
+#person call rate with standard 0.1 threshold
+#write output to step5/step5
+os.system("plink --bfile out/step2/step2_0/step2_0 --mind 0.1 --make-bed --out out/step3/step3")
+
+### STEP 4 - Hardy Weinberg ###
+
+#make directory for hardy-weinberg output
+os.system("mkdir out/step4")
+
+#write hardy-weinberg test to step4
+#where SNPs with p < 1e-6 are excluded
+os.system("plink --bfile out/step2/step2_0/step2_0 --hwe 1e-6 --make-bed -out out/step4/step4")
