133e1e7fd5695be0ab6e002f6703327e6dc72301
jaredcara <jcara@luc.edu>
2017-04-26 11:55:41 -0500

    Delete person_callrate.py

--- person_callrate.py
+++
@@ -1,9 +0,0 @@
-import os
-
-
-#make directory for person call rate check output
-os.system("mkdir out/step3")
-#person call rate with standard 0.1 threshold
-#write output to step5/step5
-os.system("plink --bfile out/step2/step2_0/step2_0 --mind 0.1 --make-bed --out out/step3/step3")
-
