2a73f3b44a77c73eeb69d016a4f13296a1bfd376
jaredcara <jcara@luc.edu>
2017-05-02 16:09:26 -0500

    Delete hardy-weinberg.py

--- old/hardy-weinberg.py
+++
@@ -1,9 +0,0 @@
-import os
-
-#make directory for hardy-weinberg output
-os.system("mkdir out/step4")
-
-#write hardy-weinberg test to step4
-#where SNPs with p < 1e-6 are excluded
-os.system("plink --bfile out/step2/step2_0/step2_0 --hwe 1e-6 --make-bed -out out/step4/step4")
-
